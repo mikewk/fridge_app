2fc1422aea8e6702491a8c1dc4637c4b6e26a14b
Mike Kok <mike.kok@gmail.com>
2022-03-14 10:44:22 +0000

    -Change UX for Get Suggestions to automatically call it on dialog open

--- src/app/food-item/add-food-item/add-food-item.component.ts
+++ src/app/food-item/add-food-item/add-food-item.component.ts
@@ -1,11 +1,11 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog'
-import {FoodItem, QL_Storage} from "../../graphql.types";
+import {FoodItem, QL_Storage, Suggestion} from "../../graphql.types";
 import {COMMA, ENTER, SPACE} from "@angular/cdk/keycodes";
 import {MatChipInputEvent} from "@angular/material/chips";
 import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 import {FoodItemService} from "../../_services/food-item.service";
-import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatSnackBar, MatSnackBarRef} from "@angular/material/snack-bar";
 
 
 /**
@@ -22,6 +22,8 @@ export class AddFoodItemComponent implements OnInit {
   editing = false;
   storage: QL_Storage;
   safeImage: SafeResourceUrl;
+  suggestion?: Suggestion;
+  snackBarRef?: MatSnackBarRef<any>;
   waitingForSuggestion = false;
   readonly separatorKeyCodes = [ENTER, COMMA, SPACE] as const;
 
@@ -32,10 +34,12 @@ export class AddFoodItemComponent implements OnInit {
       //Make a copy of the food item
       this.foodItem = Object.assign({}, data.foodItem);
       this.foodItem.tags = [...data.foodItem.tags];
+      //If we have a food item, then we're editing not adding
       this.editing = true;
     }
     else
     {
+      //No food item?  Create one and we're not editing
       this.foodItem = {name:"", tags:[]};
       this.editing = false;
     }
@@ -44,6 +48,35 @@ export class AddFoodItemComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    //Go get our suggestions immediately
+    this.foodItemService.getSuggestions(this.safeImage.toString()).subscribe(
+      {
+        next: data => {
+          //If the API call was successful
+            if (data.getSuggestions) {
+              //Set the suggestion
+              this.suggestion = data.getSuggestions.suggestion;
+
+              //If the user clicked "Get Suggestions" before we were ready and hasn't cancelled
+              if( this.waitingForSuggestion ) {
+                //Dismiss the snackback if it's there (It should be, but with auto-suggestions on maybe not)
+                this.snackBarRef?.dismiss();
+
+                //Set the tags to our suggestion
+                this.foodItem.name = this.suggestion.name;
+                this.foodItem.tags = this.suggestion.tags;
+              }
+            } else {
+              console.log(data);
+            }
+          },
+          error: err => {
+          this.waitingForSuggestion = false;
+          this.snackBarRef?.dismiss();
+            console.log(err);
+          }
+        }
+    );
   }
 
   /**
@@ -73,37 +106,25 @@ export class AddFoodItemComponent implements OnInit {
   }
 
   getSuggestion() {
-    this.waitingForSuggestion = true;
-    let snackBarRef = this.snackBar.open("Getting suggestions...","Cancel");
-    snackBarRef.onAction().subscribe(
-      {
-        next: () => {
-          this.waitingForSuggestion = false;
-          snackBarRef.dismiss();
-        }
-      }
-    );
-
-    this.foodItemService.getSuggestions(this.safeImage.toString()).subscribe(
-      {
-        next: data => {
-          snackBarRef.dismiss();
-          //If the API call was successful
-            if (data.getSuggestions && this.waitingForSuggestion) {
-              this.waitingForSuggestion = false;
-              //Check to see if they've cancelled the suggestion
-              this.foodItem.name = data.getSuggestions.suggestion.name;
-              this.foodItem.tags = data.getSuggestions.suggestion.tags;
-            } else {
-              console.log(data);
-            }
-          },
-          error: err => {
-          this.waitingForSuggestion = false;
-          snackBarRef.dismiss();
-            console.log(err);
+    //If we haven't gotten the suggestions back from the API
+    if( !this.suggestion ) {
+      //Show a snackback
+      this.waitingForSuggestion = true;
+      this.snackBarRef = this.snackBar.open("Getting suggestions...", "Cancel");
+      this.snackBarRef.onAction().subscribe(
+        {
+          next: () => {
+            //If they cancel, change our flag and dismiss the snackbar
+            this.waitingForSuggestion = false;
+            this.snackBarRef?.dismiss();
           }
         }
-    );
+      );
+    }
+    else {
+      //Fill them in since we've got em
+      this.foodItem.name = this.suggestion.name;
+      this.foodItem.tags = this.suggestion.tags;
+    }
   }
 }
