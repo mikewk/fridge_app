1f7e8c18aec3578f09742a4277935de87ab5a273
Mike Kok <mike.kok@gmail.com>
2022-04-13 01:44:30 +0100

    Change production target url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
-const graphql_base_uri = "fridge.michealkok.com/api"
+const graphql_base_uri = "fridgetracker.co/api"
 export const environment = {
   production: true,
   image_base_url: "https://d33stfk6gvmz1c.cloudfront.net/",
-  invite_base_url: "https://fridge.michealkok.com/",
+  invite_base_url: "https://fridgetracker.co/",
   graphql_wsUri: "wss://"+graphql_base_uri+"/ws",
   graphql_httpUri: "https://"+graphql_base_uri+"/graphql"
 };
